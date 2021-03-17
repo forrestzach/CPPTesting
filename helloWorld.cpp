e262e53aaad20beaf8f601cb82bbaa232d8c958d
forrestzach <forrestczach@gmail.com>
2021-03-17 18:30:40 -0400

    bad tutor

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -3,5 +3,5 @@
 //I have substantially more cpp experience than this I promise, this is just to test out github
 int main(){
     std::cout << "Hello world" << std::endl;
-    std::cout << "Dogs are cool" << std::endl;
+    std::cout << "Dogs are only sort of cool" << std::endl;
 }
