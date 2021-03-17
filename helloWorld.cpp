4d3627803fe3bfb14e7b4aa2dc6b051a4848520e
forrestzach <forrestczach@gmail.com>
2021-03-17 18:26:11 -0400

    initial commit

---
+++ helloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+//I have substantially more cpp experience than this I promise, this is just to test out github
+int main(){
+    std::cout << "Hello world" << std::endl;
+    std::cout << "Dogs are cool" << std::endl;
+}
